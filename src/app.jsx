640dfe03a085f3764806f7a6db4d0b9fe64c042c
glass <glass.xcx@gmail.com>
2022-08-27 14:03:08 +0900

    TailwindCSS

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,25 @@
 import React from "react";
-import "./app.css";
 
-function App() {
-  return <h1>hello :)</h1>;
-}
+const App = () => {
+  return (
+    <>
+      <h1 className="text-green-300 bg-blue-500">React</h1>
+      <p>How to setting the project</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="text-green-300">dd</div>
+      <div className="bg-red-700">Mint-Coding</div>
+
+      <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        ...
+      </button>
+
+      <div className="px-4 text-yellow-100 bg-blue-200"></div>
+
+      <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        ...
+      </button>
+    </>
+  );
+};
 
 export default App;
